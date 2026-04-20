e816b9759c2b18316908d64e6c3ec5e8d38bd21c
Thanh Nguyen Van <thanh.nguyen2@rikai.technology>
2026-04-20 15:32:53 +0700

    solve problem 3

--- ngay3/bai1.py
+++ ngay3/bai1.py
@@ -2,9 +2,13 @@ class Student:
     def __init__(self, name, score):
         self.name = name
         self.score = score
-
+            
     def is_passed(self):
         return self.score >= 5
+
+class Account():
+    def __init__(self, balance):
+        self.__balance = balance
 
 s = Student("An", 9)
 
@@ -13,3 +17,7 @@ print(s.name)
 s2 = Student("Binh", 4.5)
 
 print(s2.is_passed())
+
+acc = Account(100)
+
+print(acc.__balance())
