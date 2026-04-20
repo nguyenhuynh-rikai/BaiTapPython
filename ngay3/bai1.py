f105e331b2019fbcfe1b2c61927262a8f289abbe
Thanh Nguyen Van <thanh.nguyen2@rikai.technology>
2026-04-20 15:03:42 +0700

    solve problem 2

--- ngay3/bai1.py
+++ ngay3/bai1.py
@@ -3,6 +3,13 @@ class Student:
         self.name = name
         self.score = score
 
+    def is_passed(self):
+        return self.score >= 5
+
 s = Student("An", 9)
 
 print(s.name)
+
+s2 = Student("Binh", 4.5)
+
+print(s2.is_passed())
