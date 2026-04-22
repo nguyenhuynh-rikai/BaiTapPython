392fed9ab8289f59edc53f8f34f450a99090b2d8
Thanh Nguyen Van <thanh.nguyen2@rikai.technology>
2026-04-22 10:39:25 +0700

    solve problem 7

---
+++ ngay5/bai7.py
@@ -0,0 +1,18 @@
+class CountDown:
+    def __init__(self, start):
+        self.current = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= 0:
+            raise StopIteration   # 🔥 kết thúc tại đây
+        val = self.current
+        self.current -= 1
+        return val
+
+cd = CountDown(3)
+
+for x in cd:
+    print(x)
