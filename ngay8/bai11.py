2b6068a55a346390919a3bf75d7339d15eec7e90
Nguyen Huynh Nhat <Nguyen.huynh@rikai.technology>
2026-04-28 11:02:56 +0700

    bai11lan1

--- ngay8/bai11.py
+++ ngay8/bai11.py
@@ -1,18 +1,8 @@
-from shapely.geometry import Polygon
 import geopandas as gpd
 
-poly = Polygon([
-    (108.15, 16.00),
-    (108.25, 16.00),
-    (108.25, 16.10),
-    (108.15, 16.10)
-])
+gdf = gpd.read_file("data/gadm41_VNM_3.shp")
+gdf = gdf.to_crs("EPSG:3857")
 
-gdf = gpd.GeoDataFrame(geometry=[poly], crs="EPSG:4326")
+gdf["area"] = gdf.geometry.area
 
-gdf_utm = gdf.to_crs(epsg=32648)
-
-
-gdf_utm["area_m2"] = gdf_utm.area
-
-print(gdf_utm["area_m2"])
+print(gdf["area"])
