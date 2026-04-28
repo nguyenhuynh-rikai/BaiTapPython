b0549f4fd59915c811a25443c104dc58b3248aea
Nguyen Huynh Nhat <Nguyen.huynh@rikai.technology>
2026-04-28 10:59:48 +0700

    bai5lan1

--- ngay8/bai5.py
+++ ngay8/bai5.py
@@ -1,18 +1,14 @@
-from shapely.geometry import Polygon
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-poly = Polygon([
-    (108.15, 16.00),
-    (108.25, 16.00),
-    (108.25, 16.10),
-    (108.15, 16.10)
-])
+gdf = gpd.read_file("data/gadm41_VNM_3.shp")
 
-gdf = gpd.GeoDataFrame(
-    {"name": ["Da Nang Area"], "geometry": [poly]},
-    crs="EPSG:4326"
+print(gdf.head())
+
+gdf.plot(
+    edgecolor="black",
+    linewidth=0.3,
+    figsize=(10, 10)
 )
 
-gdf.plot()
 plt.show()
