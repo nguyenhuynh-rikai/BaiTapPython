e6350c9a23937b8a2b1a8dca0b5d940fa6ecdf02
Nguyen Huynh Nhat <Nguyen.huynh@rikai.technology>
2026-04-28 10:58:02 +0700

    bai3lan1

--- ngay8/bai3.py
+++ ngay8/bai3.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-gdf = gpd.read_file("data/my_map.shp")
+gdf = gpd.read_file("data/gadm41_VNM_3.shp")
 # gdf = gpd.read_file("data/test.geojson")
 
 
