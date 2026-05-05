a10621a3f0f6da7aee7e6b47586b33271fa10550
Nguyen Huynh Nhat <Nguyen.huynh@rikai.technology>
2026-05-05 13:12:48 +0700

    bai14fix2

--- ngay11/bai14.py
+++ ngay11/bai14.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestBasic(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(1 + 3, 4)
+        self.assertEqual(1 + 2, 3)
 
 if __name__ == "__main__":
     unittest.main()
