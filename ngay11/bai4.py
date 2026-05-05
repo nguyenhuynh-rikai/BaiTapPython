e94f296605b726e803ff71e1d50bcdce0595afe2
thanhnguyen2-boop <thanh.nguyen2@rikai.technology>
2026-05-05 10:13:51 +0700

    solve problem 4

---
+++ ngay11/bai4.py
@@ -0,0 +1,4 @@
+# One-file (Clean): pyinstaller --onefile 04_packaging_modes.py
+# One-folder (Faster launch): pyinstaller --onedir 04_packaging_modes.py
+
+print("Check the 'dist' folder to see the difference!")
