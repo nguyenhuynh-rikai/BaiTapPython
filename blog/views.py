882ed5afe918d51daf430f10a7653f69bcf506ad
thanhnguyen2-boop <thanh.nguyen2@rikai.technology>
2026-05-06 11:43:32 +0700

    solve problem 6

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import datetime
 
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello_world(request):
+    now = datetime.datetime.now()
+    html = f"<html><body><h1>Bây giờ là: {now}</h1></body></html>"
+    return HttpResponse(html)
 # Create your views here.
