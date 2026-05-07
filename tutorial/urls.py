d2dea3afd398b90eefad295e88c0e2be2fa82834
thanhnguyen2-boop <thanh.nguyen2@rikai.technology>
2026-05-07 10:53:51 +0700

    solve problem 3

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,15 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from tutorial.quickstart import views
-from tutorial.quickstart.views import UserViewSet, GroupViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include(router.urls)),
     path('api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include('tutorial.api.urls')),
 ]
