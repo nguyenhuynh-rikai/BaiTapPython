87b3950a94584023a06e5f7d9efc41e37f68eaa9
thanhnguyen2-boop <thanh.nguyen2@rikai.technology>
2026-05-11 09:44:06 +0700

    feat: solve problem 17

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -64,8 +64,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
+        'anon': '5/minute',
+        'user': '10/minute'
     }
 }
 
